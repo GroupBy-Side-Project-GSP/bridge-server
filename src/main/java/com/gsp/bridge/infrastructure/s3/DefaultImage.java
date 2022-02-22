de0f239b440be867e75131876c7891a7937bc113
lyutvs <lyutvs@gmail.com>
2022-02-23 01:43:55 +0900

    📑:: defaultImage 추가

---
+++ src/main/java/com/gsp/bridge/infrastructure/s3/DefaultImage.java
@@ -0,0 +1,9 @@
+package com.gsp.bridge.infrastructure.s3;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DefaultImage {
+	public static final String USER_PROFILE_IMAGE = "'a'";
+}
