d4b2c300f0d3efcefd6011b5280a077a1dbe4e51
Hyeokwoo Kwon <pllapallpal@gmail.com>
2022-02-23 22:41:58 +0900

    📑 :: (#15) ScheduleRepository 추가

---
+++ src/main/java/com/gsp/bridge/domain/schedule/domain/repostiory/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package com.gsp.bridge.domain.schedule.domain.repostiory;
+
+import com.gsp.bridge.domain.schedule.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}
