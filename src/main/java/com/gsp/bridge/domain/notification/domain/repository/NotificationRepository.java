bbaf1e26245d082ee383232e0182de44d134313f
skmn3 <sangjin170202@naver.com>
2022-02-24 00:56:42 +0900

    ♻️ :: @Repository 추가

--- src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationRepository.java
+++ src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationRepository.java
@@ -2,7 +2,8 @@ package com.gsp.bridge.domain.notification.domain.repository;
 
 import com.gsp.bridge.domain.notification.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
 }
