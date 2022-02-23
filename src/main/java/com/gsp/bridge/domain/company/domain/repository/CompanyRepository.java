ae13e9eef7d3ca9735b2528eb532ba42760509ff
lyutvs <lyutvs@gmail.com>
2022-02-23 18:20:26 +0900

    ⚡️:: Repository 명시

--- src/main/java/com/gsp/bridge/domain/company/domain/repository/CompanyRepository.java
+++ src/main/java/com/gsp/bridge/domain/company/domain/repository/CompanyRepository.java
@@ -2,7 +2,8 @@ package com.gsp.bridge.domain.company.domain.repository;
 
 import com.gsp.bridge.domain.company.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
