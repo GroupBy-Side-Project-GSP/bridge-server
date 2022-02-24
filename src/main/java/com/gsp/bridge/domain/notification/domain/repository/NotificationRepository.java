73806f7fb72da08647f7e72ee9c1d4f7245c8089
skmn3 <sangjin170202@naver.com>
2022-02-24 16:18:29 +0900

    📑 :: 파일명 바꿈

--- src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationRepository.java
+++ src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationRepository.java
@@ -1,9 +1,9 @@
 package com.gsp.bridge.domain.notification.domain.repository;
 
-import com.gsp.bridge.domain.notification.domain.Notification;
+import com.gsp.bridge.domain.notification.domain.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
 }
