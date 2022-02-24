57c803faabab3624dc3734c16e3ff408b336531a
rosel0314 <rosel0314@mail.hongik.ac.kr>
2022-02-25 03:22:57 +0900

    📑 :: BusinessCollaborationRepository 추가

---
+++ src/main/java/com/gsp/bridge/domain/businesscollaboration/domain/repository/BusinessCollaborationRepository.java
@@ -0,0 +1,7 @@
+package com.gsp.bridge.domain.businesscollaboration.domain.repository;
+
+import com.gsp.bridge.domain.businesscollaboration.domain.BusinessCollaboration;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BusinessCollaborationRepository extends JpaRepository<BusinessCollaboration, Long> {
+}
