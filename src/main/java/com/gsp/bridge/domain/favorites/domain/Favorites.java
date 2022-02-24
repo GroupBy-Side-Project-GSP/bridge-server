9e8e5709e71acd70a4c7289eb12df8075588b655
Hyeokwoo Kwon <pllapallpal@gmail.com>
2022-02-24 11:27:24 +0900

    📑 :: Favorites 추가

---
+++ src/main/java/com/gsp/bridge/domain/favorites/domain/Favorites.java
@@ -0,0 +1,33 @@
+package com.gsp.bridge.domain.favorites.domain;
+
+import com.gsp.bridge.domain.company.domain.Company;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Favorites {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company-id")
+    private Company company;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company-id")
+    private Company targetCompany;
+
+    @Builder
+    public Favorites(Company company, Company targetCompany) {
+        this.company = company;
+        this.targetCompany = targetCompany;
+    }
+}
