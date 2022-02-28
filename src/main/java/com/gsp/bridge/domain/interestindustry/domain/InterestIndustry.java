1703a7e142b8bb5689e9d338dea6bfb90060a343
pllap <pllapallpal@gmail.com>
2022-02-28 21:10:06 +0900

    ♻ :: InterestIndustry 엔티티명 변경

--- src/main/java/com/gsp/bridge/domain/interestindustry/domain/InterestIndustry.java
+++ src/main/java/com/gsp/bridge/domain/interestindustry/domain/InterestIndustry.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Table(name = "tbl_interest_industry")
 public class InterestIndustry {
 
     @Id
