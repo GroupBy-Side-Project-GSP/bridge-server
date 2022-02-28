7cd20b3c3d5ca6c6615040251d111788433607d6
pllap <pllapallpal@gmail.com>
2022-02-28 13:52:38 +0900

    ♻ :: FileEntity 어노테이션 순서 변경

--- src/main/java/com/gsp/bridge/domain/file/domain/FileEntity.java
+++ src/main/java/com/gsp/bridge/domain/file/domain/FileEntity.java
@@ -10,8 +10,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Getter
-@Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 @Table(name = "tbl_file")
 public class FileEntity {
 
