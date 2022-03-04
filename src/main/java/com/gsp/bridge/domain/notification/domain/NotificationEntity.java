e51f85cb7d319da603216f3e9959d4d9adf5fdea
skmn3 <sangjin170202@naver.com>
2022-03-04 14:48:35 +0900

    ♻ :: 엔티티명 변경

--- src/main/java/com/gsp/bridge/domain/notification/domain/NotificationEntity.java
+++ src/main/java/com/gsp/bridge/domain/notification/domain/NotificationEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "notification")
+@Table(name = "tbl_notification")
 public class NotificationEntity {
 
     @Id
