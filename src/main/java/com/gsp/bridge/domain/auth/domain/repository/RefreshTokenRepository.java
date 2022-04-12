391db72cb7ca22c8a798e08dfb3b5585daf467e5
lyutvs <lyutvs@gmail.com>
2022-04-12 15:41:49 +0900

    ⚡️ :: 일단 지움

--- src/main/java/com/gsp/bridge/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/gsp/bridge/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -1,10 +1,8 @@
-package com.walkhub.walkhub.domain.auth.domain.repository;
+package com.gsp.bridge.domain.auth.domain.repository;
 
-import com.walkhub.walkhub.domain.auth.domain.RefreshToken;
+import com.gsp.bridge.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-    Optional<RefreshToken> findByToken(String token);
 }
