0923901ad380699d3fa47a4df23f916adb16180b
lyutvs <lyutvs@gmail.com>
2022-04-12 15:48:52 +0900

    📑 :: ExpiredJwtException

---
+++ src/main/java/com/gsp/bridge/domain/auth/exception/ExpiredJwtException.java
@@ -0,0 +1,14 @@
+package com.gsp.bridge.domain.auth.exception;
+
+import com.gsp.bridge.global.error.exception.CustomException;
+import com.gsp.bridge.global.error.exception.ErrorCode;
+
+public class ExpiredJwtException extends CustomException {
+
+    public static final CustomException EXCEPTION =
+            new ExpiredJwtException();
+
+    private ExpiredJwtException() {
+        super(ErrorCode);
+    }
+}
