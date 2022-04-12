95a41710d49c554c7a12636b7eed73765e580319
lyutvs <lyutvs@gmail.com>
2022-04-12 15:49:10 +0900

    ⚡️ :: ErrorCode

--- src/main/java/com/gsp/bridge/global/error/exception/ErrorCode.java
+++ src/main/java/com/gsp/bridge/global/error/exception/ErrorCode.java
@@ -9,7 +9,11 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
-    COMPANY_NOT_FOUND(404, "COMPANY-404-1", "Company Not Found.");
+    COMPANY_NOT_FOUND(404, "COMPANY-404-1", "Company Not Found."),
+
+    EXPIRED_TOKEN(401, "TOKEN-401-1", "Expired Token"),
+    INVALID_TOKEN(401, "TOKEN-401-2", "Invalid Token");
+
 
     private final int status;
     private final String code;
