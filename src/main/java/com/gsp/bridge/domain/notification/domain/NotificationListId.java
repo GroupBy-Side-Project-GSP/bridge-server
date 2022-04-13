c7a65a777fda9e194766a42c53905f2a78a7556b
lyutvs <lyutvs@gmail.com>
2022-04-13 22:04:21 +0900

    📑 :: NotificationListId

---
+++ src/main/java/com/gsp/bridge/domain/notification/domain/NotificationListId.java
@@ -0,0 +1,18 @@
+package com.gsp.bridge.domain.notification.domain;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@EqualsAndHashCode
+public class NotificationListId implements Serializable {
+
+    private Long notificationEntity;
+
+    private Long company;
+
+}
