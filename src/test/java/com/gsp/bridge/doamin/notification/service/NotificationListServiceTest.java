28115b36a3abdc65ceaa732ae4c17b4a4f9de7e1
lyutvs <lyutvs@gmail.com>
2022-04-14 20:10:55 +0900

    ⚡️ :: ExtendWith 안넣음 ㅎㅎ..

--- src/test/java/com/gsp/bridge/doamin/notification/service/NotificationListServiceTest.java
+++ src/test/java/com/gsp/bridge/doamin/notification/service/NotificationListServiceTest.java
@@ -2,26 +2,26 @@ package com.gsp.bridge.doamin.notification.service;
 
 import com.gsp.bridge.domain.company.domain.entity.Company;
 import com.gsp.bridge.domain.company.facade.CompanyFacade;
-import com.gsp.bridge.domain.notification.domain.NotificationEntity;
 import com.gsp.bridge.domain.notification.domain.NotificationList;
 import com.gsp.bridge.domain.notification.domain.repository.NotificationListRepository;
 import com.gsp.bridge.domain.notification.presentation.response.QueryNotificationListResponse;
 import com.gsp.bridge.domain.notification.service.QueryNotificationListService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 public class NotificationListServiceTest {
 
     @Mock
@@ -40,10 +40,7 @@ public class NotificationListServiceTest {
         String accountId = "Test AccountId";
         Page<NotificationList> notificationList = Page.empty(Pageable.unpaged());
         //given
-        Company company = Company.builder()
-                .companyName(name)
-                .accountId(accountId)
-                .build();
+        Company company = Company.builder().build();
         Pageable pageable = PageRequest.of(5, 5);
         QueryNotificationListResponse response = new QueryNotificationListResponse(new ArrayList<>());
 
