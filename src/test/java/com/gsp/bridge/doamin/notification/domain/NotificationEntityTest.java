3d7b02add2674ecac15db3022c43080d74d8ed00
lyutvs <lyutvs@gmail.com>
2022-04-14 22:25:38 +0900

    🧪 :: 알림 확인

--- src/test/java/com/gsp/bridge/doamin/notification/domain/NotificationEntityTest.java
+++ src/test/java/com/gsp/bridge/doamin/notification/domain/NotificationEntityTest.java
@@ -4,11 +4,21 @@ import com.gsp.bridge.domain.company.domain.entity.Company;
 import com.gsp.bridge.domain.notification.domain.NotificationEntity;
 import com.gsp.bridge.domain.notification.domain.NotificationList;
 import com.gsp.bridge.domain.notification.domain.type.Type;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class NotificationEntityTest {
+
+    private final String title = "Test title";
+
+    private final String content = "Test content";
+
+    private final boolean isRead = false;
+
+    private final NotificationList notification = NotificationList.builder().build();
 
     @Test
     @DisplayName("Builder를 할용하여 NotificationEntity객체를 생성하는 메소드")
@@ -37,5 +47,18 @@ class NotificationEntityTest {
                 () -> Assertions.assertEquals(notification, notificationList.getNotificationEntity()),
                 () -> Assertions.assertEquals(company, notificationList.getCompany())
         );
+    }
+
+    @Order(0)
+    @Test
+    void 알림_읽음여부() {
+        assertThat(isRead).isEqualTo(notification.getIsRead());
+    }
+
+    @Order(1)
+    @Test
+    void 알림_확인() {
+        notification.updateNotificationIsRead();
+        assertTrue(notification.getIsRead());
     }
 }
