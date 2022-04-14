215b83bd541b46d3dfd3c3dbeea969c8907d1d6d
lyutvs <lyutvs@gmail.com>
2022-04-14 17:25:02 +0900

    ⚡️ :: NotificationListResponse

--- src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationListRepository.java
+++ src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationListRepository.java
@@ -1,8 +1,13 @@
 package com.gsp.bridge.domain.notification.domain.repository;
 
+import com.gsp.bridge.domain.company.domain.entity.Company;
 import com.gsp.bridge.domain.notification.domain.NotificationList;
 import com.gsp.bridge.domain.notification.domain.NotificationListId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationListRepository extends CrudRepository<NotificationList, NotificationListId> {
+
+    Page<NotificationList> findByCompany(Company company, Pageable pageable);
 }
