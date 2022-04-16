55d67436bebebd452bf44e8da9d7d5b3058600d5
lyutvs <lyutvs@gmail.com>
2022-04-16 18:42:07 +0900

    ⚡️ :: NotificationController

--- src/main/java/com/gsp/bridge/domain/notification/presentation/NotificationController.java
+++ src/main/java/com/gsp/bridge/domain/notification/presentation/NotificationController.java
@@ -1,12 +1,12 @@
 package com.gsp.bridge.domain.notification.presentation;
 
 import com.gsp.bridge.domain.notification.presentation.response.QueryNotificationListResponse;
+import com.gsp.bridge.domain.notification.service.NotificationReadService;
 import com.gsp.bridge.domain.notification.service.QueryNotificationListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,10 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 
     private final QueryNotificationListService queryNotificationListService;
+    private final NotificationReadService notificationReadService;
 
     @GetMapping
     public QueryNotificationListResponse notificationList(Pageable pageable) {
         return queryNotificationListService.execute(pageable);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{notification-id}")
+    public void notificationRead(@PathVariable("notification-id") Long notificationId) {
+        notificationReadService.execute(notificationId);
+    }
 }
