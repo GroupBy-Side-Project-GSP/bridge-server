bdb7b4f77b677820818eca7accb14eee365fd661
lyutvs <lyutvs@gmail.com>
2022-04-16 19:00:52 +0900

    ⚰️ :: 이상한 브런치 에서 커밋

--- src/main/java/com/gsp/bridge/domain/category/domain/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gsp.bridge.domain.category.domain.repository;
-
-import com.gsp.bridge.domain.category.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
