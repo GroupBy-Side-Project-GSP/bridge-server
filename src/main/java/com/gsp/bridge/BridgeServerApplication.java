044baaf2543d2216dc72093511e2c0480620928a
pllap <pllapallpal@gmail.com>
2022-04-18 01:48:07 +0900

    ♻ :: `JwtProperties` properties 사용 명시

--- src/main/java/com/gsp/bridge/BridgeServerApplication.java
+++ src/main/java/com/gsp/bridge/BridgeServerApplication.java
@@ -1,9 +1,12 @@
 package com.gsp.bridge;
 
+import com.gsp.bridge.global.security.jwt.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableConfigurationProperties(value = {JwtProperties.class})
 @EnableJpaAuditing
 @SpringBootApplication
 public class BridgeServerApplication {
