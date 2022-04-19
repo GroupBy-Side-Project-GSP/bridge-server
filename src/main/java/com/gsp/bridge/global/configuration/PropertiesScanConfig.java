d1bb9795e268d398d55bc876f42efac6a6f0eb6d
pllap <pllapallpal@gmail.com>
2022-04-19 12:12:48 +0900

    ♻ :: AppConfig 파일 분할

--- src/main/java/com/gsp/bridge/global/configuration/PropertiesScanConfig.java
+++ src/main/java/com/gsp/bridge/global/configuration/PropertiesScanConfig.java
@@ -2,10 +2,8 @@ package com.gsp.bridge.global.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @ConfigurationPropertiesScan(basePackages = "com.gsp.bridge")
 @Configuration
-public class AppConfig {
+public class PropertiesScanConfig {
 }
