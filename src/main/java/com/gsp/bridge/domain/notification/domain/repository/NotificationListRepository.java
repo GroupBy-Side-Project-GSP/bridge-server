bd7058f65d71a1a06ea3c54ba2d0782f40e43447
lyutvs <lyutvs@gmail.com>
2022-04-26 14:32:13 +0900

    ♻️ :: UserRepository로 변경

--- src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationListRepository.java
+++ src/main/java/com/gsp/bridge/domain/notification/domain/repository/NotificationListRepository.java
@@ -1,6 +1,6 @@
 package com.gsp.bridge.domain.notification.domain.repository;
 
-import com.gsp.bridge.domain.company.domain.entity.Company;
+import com.gsp.bridge.domain.user.domain.User;
 import com.gsp.bridge.domain.notification.domain.NotificationList;
 import com.gsp.bridge.domain.notification.domain.NotificationListId;
 import org.springframework.data.domain.Page;
@@ -9,5 +9,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface NotificationListRepository extends CrudRepository<NotificationList, NotificationListId> {
 
-    Page<NotificationList> findByCompany(Company company, Pageable pageable);
+    Page<NotificationList> findByCompany(User user, Pageable pageable);
 }
--- src/main/java/com/gsp/bridge/domain/user/facade/UserFacade.java
+++ src/main/java/com/gsp/bridge/domain/user/facade/UserFacade.java
@@ -1,7 +1,7 @@
 package com.gsp.bridge.domain.user.facade;
 
-import com.gsp.bridge.domain.user.domain.entity.User;
-import com.gsp.bridge.domain.user.domain.entity.repository.UserRepository;
+import com.gsp.bridge.domain.user.domain.User;
+import com.gsp.bridge.domain.user.domain.UserRepository;
 import com.gsp.bridge.domain.user.exception.CompanyNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
